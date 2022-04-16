0f74bd6233ab9ea5ec991b006023e03c55270146
mithi9 <96mithi@gmail.com>
2022-04-15 23:30:07 -0600

    Create a funciotn tht compares user choice and computer choice, applies a logic, and returns a victory, draw, or defeat message. Increase sum by a corresponding value of the draw, loss, or win.

--- script.js
+++ script.js
@@ -7,12 +7,48 @@ let choice = ['rock','paper','scissors'];
 let playerSelection = prompt('Pick between rock, papaer, or scissors','').toLowerCase();
 let computerSelection = computerPlay();
 
+let sum = playRound(playerSelection,computerSelection);
 
+console.log(sum);
 
-console.log(playerSelection);
-
-function playerPlay(){
+function playRound(playerSelection, computerSelection){
     
+    if (playerSelection==computerSelection){
+        alert("Draw!");
+        return 0;
+    }
+
+    else if (playerSelection=='rock'&&computerSelection=='scissors'){
+        alert('You win!');
+        return 1;
+    }
+    else if (playerSelection=='rock'&&computerSelection=='paper'){
+        alert('You lose!');
+        return -1;
+    }
+
+    else if (playerSelection=='paper'&&computerSelection=='rock'){
+        alert('You win!');
+        return 1;
+    }
+    else if (playerSelection=='paper'&&computerSelection=='scissors'){
+        alert('You lose!');
+        return -1;
+    }
+
+    else if (playerSelection=='scissors'&&computerSelection=='paper'){
+        alert('You win!');
+        return 1;
+    }
+    else if (playerSelection=='scissors'&&computerSelection=='rock'){
+        alert('You lose!');
+        return -1;
+    }
+    
+    else{
+        return;
+    }
+
 
 
 }
